5c81de14f8ae4a46188add9e307eb8e725f5543b
Olaf Lüke <olaf@tinkerforge.com>
2018-08-24 10:28:01 +0200

    Use NPGOOD pin to enable/disable boost

--- software/src/bq24075.c
+++ software/src/bq24075.c
@@ -25,23 +25,29 @@
 #include "configs/config_rpi.h"
 
 void bq24075_init(void) {
-    XMC_GPIO_CONFIG_t output_low = {
-	    .mode = XMC_GPIO_MODE_OUTPUT_PUSH_PULL,
-        .output_level = XMC_GPIO_OUTPUT_LEVEL_LOW
-    };
+	XMC_GPIO_CONFIG_t output_low = {
+		.mode = XMC_GPIO_MODE_OUTPUT_PUSH_PULL,
+		.output_level = XMC_GPIO_OUTPUT_LEVEL_LOW
+	};
 
-    XMC_GPIO_CONFIG_t output_high = {
-	    .mode = XMC_GPIO_MODE_OUTPUT_PUSH_PULL,
-        .output_level = XMC_GPIO_OUTPUT_LEVEL_HIGH
-    };
+	XMC_GPIO_CONFIG_t output_high = {
+		.mode = XMC_GPIO_MODE_OUTPUT_PUSH_PULL,
+		.output_level = XMC_GPIO_OUTPUT_LEVEL_HIGH
+	};
 
-	XMC_GPIO_Init(BQ24075_NSYSOFF_PIN, &output_low);
-    XMC_GPIO_Init(BQ24075_NEN_PIN, &output_low);
-    XMC_GPIO_Init(BQ24075_EN1_PIN, &output_low);
-    XMC_GPIO_Init(BQ24075_EN2_PIN, &output_high);
+	XMC_GPIO_CONFIG_t input = {
+		.mode = XMC_GPIO_MODE_INPUT_TRISTATE,
+		.input_hysteresis = XMC_GPIO_INPUT_HYSTERESIS_LARGE
+	};
 
+	XMC_GPIO_Init(BQ24075_NEN_PIN, &output_low);
+	XMC_GPIO_Init(BQ24075_NPGOOD_PIN, &input);
 }
 
 void bq24075_tick(void) {
-
+	if(XMC_GPIO_GetInput(BQ24075_NPGOOD_PIN)) {
+		XMC_GPIO_SetOutputLow(RPI_BOOST_EN_PIN);
+	} else {
+		XMC_GPIO_SetOutputHigh(RPI_BOOST_EN_PIN);
+	}
 }
