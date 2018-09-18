ba08919f8a5827461fefec8f41d56ed9fe5783b2
Olaf Lüke <olaf@tinkerforge.com>
2018-09-18 16:28:58 +0200

    Add support for usb/dc hotplug

--- software/src/bq24195.c
+++ software/src/bq24195.c
@@ -21,6 +21,7 @@
 
 #include "bq24195.h"
 #include "max17260.h"
+#include "voltage.h"
 
 #include "configs/config_bq24195.h"
 #include "configs/config_rpi.h"
@@ -28,6 +29,9 @@
 #include "bricklib2/os/coop_task.h"
 
 #include "bricklib2/logging/logging.h"
+
+#define BQ24195_RAW_USB_VOLTAGE_THRESHOLD (35*VOLTAGE_MAX_LENGTH*4*4095/(33*2))
+#define BQ24195_RAW_DC_VOLTAGE_THRESHOLD  (60*VOLTAGE_MAX_LENGTH*4*4095/(33*11))
 
 extern MAX17260 max17260;
 BQ24195 bq24195;
@@ -72,20 +76,66 @@ void bq24195_tick_task(void) {
 	bq24195_write_register(BQ24195_REG_CHARGE_CURRENT,     0b00100000); // Fast charge current limit 512mA
 	bq24195_write_register(BQ24195_REG_CHARGE_TERMINATION, 0b10001010); // Turn watchdog off
 
+	uint32_t print_time = 0;
+	bool last_usb_low = true;
+	uint32_t last_usb_time = 0;
+	bool last_dc_low = true;
+	uint32_t last_dc_time = 0;
+	uint32_t wait_time = 0;
 	while(true) {
 		if(max17260.new_init) {
 			i2c_fifo_init(&max17260.i2c_fifo);
 			max17260.new_init = false;
 		}
 
-		uint8_t value;
-		bq24195_read_register(BQ24195_REG_SYSTEM_STATUS, &value);
-		uartbb_printf("Status: %b\n\r", value);
-		bq24195_read_register(BQ24195_REG_FAULT, &value);
-		uartbb_printf("Fault: %b\n\r", value);
-		uartbb_puts("\n\r");
+		if((wait_time == 0) || (system_timer_is_time_elapsed_ms(wait_time, 500))) {
+			wait_time = 0;
+			uint32_t raw_usb_voltage = voltage_get_usb_voltage_raw();
+			if(last_usb_low && (raw_usb_voltage > BQ24195_RAW_USB_VOLTAGE_THRESHOLD)) {
+				last_usb_low = false;
+				last_usb_time = system_timer_get_ms();
+			} else if(!last_usb_low && (raw_usb_voltage > BQ24195_RAW_USB_VOLTAGE_THRESHOLD) && (last_usb_time != 0)) {
+				if(system_timer_is_time_elapsed_ms(last_usb_time, 500)) {
+					last_usb_time = 0;
+					XMC_GPIO_SetOutputHigh(BQ24195_NCE_PIN);
+					coop_task_sleep_ms(2);
+					XMC_GPIO_SetOutputLow(BQ24195_NCE_PIN);
+				}
+			} else if(!last_usb_low && (raw_usb_voltage <= BQ24195_RAW_USB_VOLTAGE_THRESHOLD)) {
+				last_usb_low = true;
+				last_usb_time = 0;
+				wait_time = system_timer_get_ms();
+			}
 
-		coop_task_sleep_ms(1000);
+			uint32_t raw_dc_voltage = voltage_get_dc_voltage_raw();
+			if(last_dc_low && (raw_dc_voltage > BQ24195_RAW_DC_VOLTAGE_THRESHOLD)) {
+				last_dc_low = false;
+				last_dc_time = system_timer_get_ms();
+			} else if(!last_dc_low && (raw_dc_voltage > BQ24195_RAW_DC_VOLTAGE_THRESHOLD) && (last_dc_time != 0)) {
+				if(system_timer_is_time_elapsed_ms(last_dc_time, 500)) {
+					last_dc_time = 0;
+					XMC_GPIO_SetOutputHigh(BQ24195_NCE_PIN);
+					coop_task_sleep_ms(2);
+					XMC_GPIO_SetOutputLow(BQ24195_NCE_PIN);
+				}
+			} else if(!last_dc_low && (raw_dc_voltage <= BQ24195_RAW_DC_VOLTAGE_THRESHOLD)) {
+				last_dc_low = true;
+				last_dc_time = 0;
+				wait_time = system_timer_get_ms();
+			}
+		}
+
+		if(system_timer_is_time_elapsed_ms(print_time, 1000)) {
+			print_time = system_timer_get_ms();
+			uint8_t value;
+			bq24195_read_register(BQ24195_REG_SYSTEM_STATUS, &value);
+			uartbb_printf("Status: %b\n\r", value);
+			bq24195_read_register(BQ24195_REG_FAULT, &value);
+			uartbb_printf("Fault: %b\n\r", value);
+			uartbb_puts("\n\r");
+		}
+
+		coop_task_yield();
 	}
 }
 
